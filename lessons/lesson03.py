d34fb17737ed9f469580cff44743dccc42dde1ec
wesley <wesley.rochao@hotmail.com>
2024-12-26 23:31:39 -0300

    Refactor lessons: remove old lesson files and add new lesson files for improved structure and content

---
+++ lessons/lesson03.py
@@ -0,0 +1,19 @@
+#lesson 3 / Operators
+
+# print(2 + 2)
+# print(2 - 2)
+# print(2 * 2)
+# print(2 / 2)
+# print(2 % 2)
+# print(2 ** 2)
+# print(2 // 2)
+# print(2 == 2)
+# print(2 != 2)
+# print(2 > 2)
+# print(2 < 2)
+# print(2 >= 2)
+# print(2 <= 2)
+# print(2 and 2)
+# print(2 or 2)
+# print( 2+ (2*2))
+
