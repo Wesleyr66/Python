08bdc933c3f2f04324ae4329875b99e9e8862d63
wesley <wesley.rochao@hotmail.com>
2024-12-26 22:27:38 -0300

    Update lesson01.py to include date input functionality and remove commented-out code

--- lesson01.py
+++ lesson01.py
@@ -1,3 +1,17 @@
-print('Hello world')
-print('Using Python 3.7.3')
+# print('Hello world')
+# print('Using Python 3.7.3')
 
+# name = ['john', 'doe', 'smith']
+# print(name)
+# name[0] = input('Enter your first name: ')
+# name[1] = input('Enter your middle name: ')
+# name[2] = input('Enter your last name: ')
+# print(name)
+# print(name[1].upper())
+
+#Lesson 1
+
+Day = input('Enter the Day: ')
+Month = input('Enter the Month: ')
+Year = input('Enter the Year: ')
+print('The date is: ', Month,'/', Day,'/', Year)
